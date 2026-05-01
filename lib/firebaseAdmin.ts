92c11b17e6601236ed0755dd819fd529cce65188
nahj321123 <2204555@slu.edu.ph>
2026-05-02 03:28:52 +0800

    Force dynamic on sensor API and remove JSON.parse

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -1,17 +1,18 @@
-import admin from "firebase-admin"
+import * as admin from "firebase-admin";
 
 if (!admin.apps.length) {
-  const serviceAccount = JSON.parse(
-    process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string
-  )
-
-  // 🔥 FIX: restore line breaks in private key
-  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n")
-
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      // Notice we are NOT using JSON.parse here!
+      privateKey: process.env.FIREBASE_PRIVATE_KEY 
+        ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') 
+        : undefined,
+    }),
     databaseURL: process.env.FIREBASE_DATABASE_URL,
-  })
+  });
 }
 
-export const db = admin.database()
+const db = admin.database();
+export { db };
