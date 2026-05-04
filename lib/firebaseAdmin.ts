2db8a6f54abd4f5324a16cfb3f5ba243cf225398
nahj321123 <2204555@slu.edu.ph>
2026-05-04 12:31:42 +0800

    fix firebase env

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -1,11 +1,20 @@
 import admin from "firebase-admin";
 
 if (!admin.apps.length) {
+  if (
+    !process.env.FIREBASE_PROJECT_ID ||
+    !process.env.FIREBASE_CLIENT_EMAIL ||
+    !process.env.FIREBASE_PRIVATE_KEY ||
+    !process.env.FIREBASE_DB_URL
+  ) {
+    throw new Error("Missing Firebase environment variables");
+  }
+
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
     }),
     databaseURL: process.env.FIREBASE_DB_URL,
   });
